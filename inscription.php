31db5de29fb76f047f5124936ae0fba788aad215
wiss-ali <wiss.dev@outlook.fr>
2024-01-06 03:09:03 +0100

    r

--- inscription.php
+++ inscription.php
@@ -5,6 +5,10 @@ if (isset($_SESSION["username"])) {
     exit();
 }
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérez les données du formulaire
     $usernameForm = $_POST["nom_utilisateur"];

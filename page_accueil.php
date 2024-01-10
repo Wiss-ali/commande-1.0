4a1a609cb3de1bc89669b468deec710e85636da5
Farrell94 <wissem.ali@outlook.fr>
2024-01-10 16:41:32 +0100

    h

--- page_accueil.php
+++ page_accueil.php
@@ -76,7 +76,7 @@ $nombre_de_pages = ceil(count($projets) / $projets_par_page);
 <body>
 
 <header>
-    
+    <div med2>
     <h1>COMMANDES</h1>
 
     <img src="logo.wiss.png" alt="mon logo">
@@ -85,7 +85,7 @@ $nombre_de_pages = ceil(count($projets) / $projets_par_page);
         <li><a href="page_saisie_donnees.php">Nouvelle demande</a></li>
         <li><a href="#" onclick="confirmerDeconnexion()">déconnexion</a></li>
     </ul>
-    
+    </div>
     <div class="med1">
         <a href="page_saisie_donnees.php"><img src="ajouter-le-fichier.png" alt="icone ajouer fichier" class="modif"></a>
         <a href="page_accueil.php"><img src="logo.wiss.png" class="modif1" alt="mon logo"></a>
